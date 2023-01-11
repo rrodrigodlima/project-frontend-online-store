b62034c9647c32d848907cff19082d1b08940d31
KennyKoyama <kennysk1982@gmail.com>
2023-01-11 20:40:19 -0300

    Requisito 5 aprovado

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { Route, BrowserRouter, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import SearchBar from './pages/SearchBar';
 import ShoppingCart from './pages/ShoppingCart';
 
@@ -8,12 +8,10 @@ class App extends React.Component {
   render() {
     return (
       <div id="App">
-        <BrowserRouter>
-          <Switch>
-            <Route exact path="/" component={ SearchBar } />
-            <Route exact path="/shoppingcart" component={ ShoppingCart } />
-          </Switch>
-        </BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={ SearchBar } />
+          <Route exact path="/shoppingcart" component={ ShoppingCart } />
+        </Switch>
       </div>
     );
   }
